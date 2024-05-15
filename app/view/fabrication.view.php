3617fc4b1ff36100d1f93591746060c58efb0a67
j3eyy <jeremytamom0@gmail.com>
2024-05-15 17:25:34 +0200

    push

--- app/view/fabrication.view.php
+++ app/view/fabrication.view.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<html>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public\css\fabrication.css">
+    <title><?= $nomDePage ?></title>
+
+
 <body>
     <div class = "description">
     <P>Nos techniques de brassage et notre choix méticuleux d'ingrédients garantissent une bière de qualité supérieure, avec des saveurs riches et des arômes distinctifs. Chaque étape du processus est soigneusement contrôlée pour produire des bières uniques et délicieuses.
