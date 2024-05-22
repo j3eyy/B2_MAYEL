0ba43d45f5251e256142bb4062b432ffb4b3bf9c
j3eyy <jeremytamom0@gmail.com>
2024-05-22 16:19:35 +0200

    push

---
+++ brasserie.php
@@ -0,0 +1,13 @@
+<?php
+
+$nomDePage = "brasserie";
+$css_attribuee = "public/css/fabrication.css";
+
+// Construire la vue et l'injecter dans la variable $content
+ob_start();
+include 'app/view/fabrication.view.php';
+$content = ob_get_clean();
+
+// Génération du code HTML de la page à partir du layout
+include 'app/view/common/layout.php';
+?> 
