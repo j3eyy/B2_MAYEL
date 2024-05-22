41486e8d2a5a40991e3aa0c6353463f350109bd9
j3eyy <jeremytamom0@gmail.com>
2024-05-22 17:46:53 +0200

    push reglage header

--- app/view/fabrication.view.php
+++ app/view/fabrication.view.php
@@ -1,20 +1,3 @@
-<!doctype html>
-
-
-<html lang="fr">
-
-
-	<head>
-
-		<meta charset="utf-8">
-		<title>Mayel - Nos bières</title>
-        <link rel="shortcut icon" href="images/logo.png" />
-		<meta name="viewport" content="initial-scale=1.0; width=device-width">
-		<link rel="stylesheet" href="Nos bières.css" />
-        <link href="https://fonts.cdnfonts.com/css/futura-lt" rel="stylesheet">
-        <link href="https://fonts.cdnfonts.com/css/futura-std-4" rel="stylesheet">
-
-	</head>
 
 <body>
     <div class = "description">
