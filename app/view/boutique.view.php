96751bb46e0756cf27778198a448df69235d2dd9
Dhénaux Bryan <dhenaux.bryan@gmail.com>
2024-05-23 08:57:53 +0200

    model install

---
+++ app/view/boutique.view.php
@@ -0,0 +1,47 @@
+
+
+<!-- Bryan -->
+<main>
+        <!-- <style>
+            @import url('https://fonts.cdnfonts.com/css/futura-lt');
+            @import url('https://fonts.cdnfonts.com/css/futura-std-4');
+        </style> -->
+
+        <div class="intense">
+            <h1>INTENSE</h1>
+        </div>
+            
+        <div class="kiwi">
+            <a href="Pages des bières/Kiwi.html"><img src="public\images\kiwimenthe.jpg" width=20% /></a>
+        </div>
+
+        <div class="fraise">
+            <a href="Pages des bières/Fraise.html"><img src="public\images\fraiserhu.jpg" width=31% /></a>
+        </div>
+
+        <div class="yuzu">
+            <a href="Pages des bières/Yuzu.html"><img src="public\images\yuzulitchi.jpg" width=73% /></a>
+        </div>
+
+        <div class="hibiscus">
+            <a href="Pages des bières/Hibiscus.html"><img src="public\images\hibiscus.jpg" width=20% /></a>
+        </div>
+
+        <div class="chocolat">
+            <a href="Pages des bières/Chocolat.html"><img src="public\images\chocolat.jpg" width=31.2% /></a>
+        </div>
+
+        <div class="vanille">
+            <a href="Pages des bières/Vanille.html"><img src="public\images\vanille.jpg" width=73.5% /></a>
+        </div>
+
+        <div class="prestige">
+            <h1>PRESTIGE</h1>
+        </div>
+
+        <div class="safran">
+            <a href="Pages des bières/Safran.html"><img src="public\images\safran.jpg" width=30%/></a>
+        </div>
+
+        </br>
+    </main>
