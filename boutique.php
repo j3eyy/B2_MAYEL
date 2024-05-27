83b095fd85b971c82e7909dc8e96a2362c291042
Dhénaux Bryan <dhenaux.bryan@gmail.com>
2024-05-27 15:39:28 +0200

    Update boutique.php

--- boutique.php
+++ boutique.php
@@ -6,7 +6,17 @@ $css = "boutique.css";
 
 include 'app/model/connexionBD.php';
 include 'app/model/biere.model.php';
-include 'app/view/common/header.php';
+include 'app/view/common/layout.php';
+
+
+if(isset($_SESSION['message'])) {
+    $message = $_SESSION['message'];
+    unset($_SESSION['message']);
+}
+
+$pdo = getDatabaseConnexion();
+$nbPages = getNbBieres($pdo, 16);
+
 
 $pdo = getDatabaseConnexion();
 
@@ -24,45 +34,4 @@ $page_title = 'boutique';
 ob_start();
 include 'app/view/boutique.view.php';
 $content = ob_get_clean();
-
-
-echo "<main>\n";
-echo "        <style>\n";
-echo "            @import url(\'https://fonts.cdnfonts.com/css/futura-lt\');\n";
-echo "            @import url(\'https://fonts.cdnfonts.com/css/futura-std-4\');\n";
-echo "        </style>\n";
-echo "    <div class=\"intense\">\n";
-echo "        <h1>INTENSE</h1>\n";
-echo "            \n";
-echo "        <div class=\"kiwi\">\n";
-echo "            <a href=\"Pages des bières/Kiwi.html\"><img src=\"app/public/images/kiwimenthe.jpg\" width=20% /></a>\n";
-echo "        </div>\n";
-echo "        <div class=\"fraise\">\n";
-echo "            <a href=\"Pages des bières/Fraise.html\"><img src=\"app/public/images/fraiserhu.jpg\" width=20% /></a>\n";
-echo "        </div>\n";
-echo "        <div class=\"yuzu\">\n";
-echo "            <a href=\"Pages des bières/Yuzu.html\"><img src=\"app/public/images/yuzulitchi.jpg\" width=20% /></a>\n";
-echo "        </div>\n";
-echo "        <div class=\"hibiscus\">\n";
-echo "            <a href=\"Pages des bières/Hibiscus.html\"><img src=\"app/public/images/hibiscus.jpg\" width=20% /></a>\n";
-echo "        </div>\n";
-echo "        <div class=\"chocolat\">\n";
-echo "            <a href=\"Pages des bières/Chocolat.html\"><img src=\"app/public/images/chocolat.jpg\" width=20% /></a>\n";
-echo "        </div>\n";
-echo "        <div class=\"vanille\">\n";
-echo "            <a href=\"Pages des bières/Vanille.html\"><img src=\"app/public/images/vanille.jpg\" width=20% /></a>\n";
-echo "        </div>\n";
-echo "        <div class=\"prestige\">\n";
-echo "            <h1>PRESTIGE</h1>\n";
-echo "        </div>\n";
-echo "        <div class=\"safran\">\n";
-echo "            <a href=\"Pages des bières/Safran.html\"><img src=\"app/public/images/safran.jpg\" width=20%/></a>\n";
-echo "        </div>\n";
-echo "    </div>\n";
-echo "        </br>\n";
-echo "    </main>\n";
-
-include 'app/view/common/footer.php';
-?>
-
 
