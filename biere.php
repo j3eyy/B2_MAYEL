3fc5f1458205a5f6db6b05867f0477626301d0d6
Dhénaux Bryan <dhenaux.bryan@gmail.com>
2024-05-27 14:23:06 +0200

    m

--- biere.php
+++ biere.php
@@ -1,16 +1,27 @@
 <?php
+session_start();
+// 1 - Récupérer, calculer ou déclarer les données
+include 'app/model/connexionBD.php';
+include 'app/model/biere.model.php';
 
-$nomDePage = "biere";
-$css = "biere.css";
+if (empty($_GET['page']) || !ctype_digit($_GET['page']) || $_GET['page'] < 1) {
+    $_SESSION["message"] = 'Erreur de connexion.';
+    header('Location: accueil.php');
+    exit;
+}
 
-// Récupérer Les bières avec la base de donnée
-include 'app/view/model/connexionBD.php';
-include 'app/biere.view.php';
+$numStudent = intval($_GET['num']);
 
-// Construire la vue et l'injecter dans la variable $content
+$student = getBiere(getDatabaseConnexion(), $numStudent);
+$page_title = 'Trombinoscope - ' . $student['firstname']  . ' ' . $student['lastname'];
+
+
+// 2 - Construire la vue et l'injecter dans la variable $content
 ob_start();
 include 'app/view/biere.view.php';
 $content = ob_get_clean();
 
-// Génération du code HTML de la page à partir du layout
+
+// 3 - Génération du code HTML de la page à partir du layout
 include 'app/view/common/layout.php';
+
