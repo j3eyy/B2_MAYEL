6d8b564c24d5151c550fd06048251ef113e31e67
Dhénaux Bryan <dhenaux.bryan@gmail.com>
2024-05-30 16:57:46 +0200

    1

--- app/view/boutique.view.php
+++ app/view/boutique.view.php
@@ -1,9 +1,6 @@
+<?php
 
-<?php foreach ($showroom as $aShowroom): ?>
 
-<div class="photo">
-        <a href=session_start();
-    var_dump($photos);
-    exit();><img src="images/kiwimenthe.jpg" width=20% /></a>
-    </div>
-<?php endforeach ?>
+    $result = mysqli_query($db, "SELECT * FROM photo");
+
+
--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-define('DB_NAME', 'contact_mayel');
+define('DB_NAME', 'site_mayel');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
