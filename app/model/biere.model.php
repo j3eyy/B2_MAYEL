a752eaa28d247220e5234c564ba20cc02dd3152f
Dhénaux Bryan <dhenaux.bryan@gmail.com>
2024-05-30 11:18:09 +0200

    Update biere.model.php

--- app/model/biere.model.php
+++ app/model/biere.model.php
@@ -7,10 +7,3 @@ function getProduits(PDO $pdo) {
     $produits = $stmt->fetchAll();
     return $produits;
 }
-
-function getDatabaseConnexion()
-{
-    $dsn = 'mysql:dbname=contact_mayel;host=localhost;charset=utf8';
-    $username = 'root';
-    $password = '';
-}
