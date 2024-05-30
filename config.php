c15d8ef07b317b4d83fd424e9a63b112e9f315ea
j3eyy <jeremytamom0@gmail.com>
2024-05-30 16:29:18 +0200

    push biere

--- config.php
+++ config.php
@@ -1,9 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-define('DB_NAME', 'contact_mayel');
-=======
 define('DB_NAME', 'site_mayel');
->>>>>>> a752eaa28d247220e5234c564ba20cc02dd3152f
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
