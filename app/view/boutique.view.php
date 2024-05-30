7397666522b5a02d0ea42773b7698c72ba2e4b5b
Dhénaux Bryan <dhenaux.bryan@gmail.com>
2024-05-30 12:10:45 +0200

    12

--- app/view/boutique.view.php
+++ app/view/boutique.view.php
@@ -1,7 +1,7 @@
 
 <?php foreach ($showroom as $aShowroom): ?>
 
-    <div class="kiwi">
-            <a href="Pages des bières/Kiwi.html"><img src="images/kiwimenthe.jpg" width=20% /></a>
-        </div>
+<div class="kiwi">
+        <a href="Pages des bières/Kiwi.html"><img src="images/kiwimenthe.jpg" width=20% /></a>
+    </div>
 <?php endforeach ?>
