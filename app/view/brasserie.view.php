f1b6e03fc158d7e8fd9bd7ae111c50774f47902c
Amin-Miladi <153726428+Amin-Miladi@users.noreply.github.com>
2024-05-30 16:17:53 +0200

    brasserie

--- app/view/brasserie.view.php
+++ app/view/brasserie.view.php
@@ -7,5 +7,52 @@
     <title>Brasserie</title>
     <link rel="stylesheet" type="text/css" href="../../public\css\brasserie.css" />
 </head>
+<body>
 
-<p> BRASSERIE</p>
+<div class="img">
+  <img src="public\images\mayel_post.jpg" alt="img Brasserie">
+</div>
+<div class="info">
+  <h1>MAYEL - BRASSERIE</h1>
+  <p>13 rue Molière | Compl. 75013, Paris 13e</p>
+</div>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.2459089257884!2d2.3499023156754704!3d48.83322117928578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e671d699ba5a3f%3A0x408ab2ae4bb5c50!2s13%20Rue%20Moli%C3%A8re%2C%2075013%20Paris%2C%20France!5e0!3m2!1sen!2sus!4v1622102379366!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+</iframe>
+
+    <main>
+        <div id="histoire">
+            <h2 class="titres">À PROPOS DE NOUS</h2>
+            <section>
+                <p>
+                    Née de l'aventure d'un groupe de jeunes passionnés à la recherche de nouvelles saveurs, Mayel a établi son restaurant bistronomique dans le Troisième arrondissement de Paris.
+
+                <br> Souhaitant magnifier leurs plats d'exception, ils ont développé une bière artisanale haut de gamme, brassée localement en collaboration avec des producteurs du monde entier.
+
+                <br> Avec une distribution exclusive, Mayel incarne la sophistication et le raffinement, offrant une expérience gustative unique.
+                </p>
+            </section>
+        </div>
+        <div id="fondateur">
+            <h2 class="titres">Fondateur</h2>
+            <ul class="img3">
+                <li><img src="public\images\amin.jpg" alt="Amin Miladi"></li></a>
+              <li><img src="public\images\charles.jpg" alt="Charles Vilaine"></li></a>
+            <li><img src="public\images\IMG_0407.JPG" alt="Jerémy Tallmon"></li></a>
+            </ul>
+            <ul class="img2">
+             <li><img src="public\images\12_bryan_dhenaux.png" alt="Bryan Dhenaux"></li></a>
+             <li><img src="public\images\12_sami_eddahbi.png"alt="Sami Eddahbi"></li></a>
+             <li><img src="public\images\12_yasmine_oulmi.png"alt="Yasmine Oulmi"></li></a>
+            </ul>
+        </div>
+    </main>
+<div class="icons">
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+  <div class="circle"></div>
+</div>
+</body>a
+</html>
