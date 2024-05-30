830530e260fea5478695a07bc5898becfa3d508d
Amin-Miladi <153726428+Amin-Miladi@users.noreply.github.com>
2024-05-30 17:55:56 +0200

    brasserie

--- app/view/brasserie.view.php
+++ app/view/brasserie.view.php
@@ -5,11 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Brasserie</title>
-    <link rel="stylesheet" type="text/css" href="../../public\css\brasserie.css" />
+    <link rel="stylesheet" type="text/css" href="public/css/brasserie.css" />
 </head>
 <body>
 
-<div class="img">
+
+<section id="emplacement">
+    <div class="img-bra">
   <img src="public\images\mayel_post.jpg" alt="img Brasserie">
 </div>
 <div class="info">
@@ -18,6 +20,8 @@
 </div>
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.2459089257884!2d2.3499023156754704!3d48.83322117928578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e671d699ba5a3f%3A0x408ab2ae4bb5c50!2s13%20Rue%20Moli%C3%A8re%2C%2075013%20Paris%2C%20France!5e0!3m2!1sen!2sus!4v1622102379366!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 </iframe>
+</section>
+
 
     <main>
         <div id="histoire">
