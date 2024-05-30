3fd3256cf99e7fa18f8fcf4036295a73acc58089
vtacharles <144895089+vtacharles@users.noreply.github.com>
2024-05-30 17:07:11 +0200

    modif config

--- app/view/contact.view.php
+++ app/view/contact.view.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>contact</title>
     <link rel="stylesheet" type="text/css" href="../../public/css/contact.css" />
+    <link rel="stylesheet" href="https://use.typekit.net/wuv4mxx.css">
 </head>
 
 <body>
