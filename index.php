5141982da98921eeecdb83c27405030e667fc467
vtacharles <144895089+vtacharles@users.noreply.github.com>
2024-06-03 16:21:53 +0200

    b

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $css= "accueil.css";
 
 // Construire la vue et l'injecter dans la variable $content
 ob_start();
-include 'app/view/index.view.php';
+include 'app/view/accueil.view.php';
 $content = ob_get_clean();
 
 // Génération du code HTML de la page à partir du layout
