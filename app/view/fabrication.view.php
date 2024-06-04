2dda9792c2e41790acf33d3a0bc067a5b8cd28f4
Dhénaux Bryan <dhenaux.bryan@gmail.com>
2024-06-04 17:26:52 +0200

    12

--- app/view/fabrication.view.php
+++ app/view/fabrication.view.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fabrication</title>
+    <link rel="stylesheet" type="text/css" href="../../public/css/fabrication.css" />
+
+</head>
 
 <body>
     <div class = "description1">
