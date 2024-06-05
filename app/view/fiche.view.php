93a177a673cc70ba27c28097ee9fe09b41c7ecea
Amin-Miladi <153726428+Amin-Miladi@users.noreply.github.com>
2024-06-05 12:22:46 +0200

    FICHE.VIEW CSS

--- app/view/fiche.view.php
+++ app/view/fiche.view.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="">
+<link rel="stylesheet" href="trombi.css">
 <div class="fiche">
     <div class="gauche">
         <figure class="big_photo">
@@ -9,12 +9,12 @@
                 $photo = 'defaut.png';
             }
             ?>
-            <img src="public/images/<?= $photo ?>" alt="photo de <?= $student['prenom'] ?> <?= $student['nom'] ?>">
+            <img src="public/images/<?= $student['Photo']?>" alt="photo de <?= $student['prenom'] ?> <?= $student['nom'] ?>">
         </figure>
     </div>
     <div class="detail">
         <p class="nom"><?= $student['prenom'] ?> <span><?= $student['nom'] ?></span></p>
-        <p class="ddn"><?= $student['date'] ?></p>
+        <p class="date"><?= $student['date'] ?></p>
         <p class="groupe">groupe <span><?= $student['groupe'] ?></span></p>
         <p>
             <?= $student['description'] ?>
