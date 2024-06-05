090efde3a7d4f8f83a6863f05a74ffa63a84bd18
Dhénaux Bryan <dhenaux.bryan@gmail.com>
2024-06-05 19:39:41 +0200

    Update boutique.view.php

--- app/view/boutique.view.php
+++ app/view/boutique.view.php
@@ -1,7 +1,7 @@
-<?php foreach ($bieres as $id => $showroom): ?>
 
+<?php foreach ($bieres as $id => $showroom): ?>
     <a href="biere.php?id=<?= $id ?>">
         <img src="<?= htmlspecialchars($showroom['image']) ?>" alt="<?= htmlspecialchars($showroom['nom']) ?>" width="20%" />
     </a>
 
-<?php endforeach; ?>
+<?php endforeach;
