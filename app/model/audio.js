d0a131b78d947766ed07086ad00f22d4c4e65b83
vtacharles <144895089+vtacharles@users.noreply.github.com>
2024-06-13 09:51:29 +0200

    modif

--- app/model/audio.js
+++ app/model/audio.js
@@ -1,31 +1,18 @@
 function play(idPlayer, control) {
-    // Affiche un message "yes" dans la console pour indiquer que la fonction a été appelée
     console.log("yes");
-    
-    // Sélectionne l'élément vidéo ou audio avec l'ID spécifié
     let player = document.querySelector('#' + idPlayer);
-    
-    // Vérifie si le lecteur est en pause
     if (player.paused) {
-        // Si le lecteur est en pause, lance la lecture
         player.play();
-        // Change le texte du bouton de contrôle en 'Pause'
         control.textContent = 'Pause';
     } else {
-        // Si le lecteur est en cours de lecture, met en pause
         player.pause();
-        // Change le texte du bouton de contrôle en 'Play'
         control.textContent = 'Play';
     }
 }
 
 function resume(idPlayer) {
-    // Sélectionne l'élément vidéo ou audio avec l'ID spécifié
+
     let player = document.querySelector('#' + idPlayer);
-    
-    // Remet le lecteur à zéro (début de la vidéo ou de l'audio)
     player.currentTime = 0;
-    
-    // Met le lecteur en pause
     player.pause();
 }
